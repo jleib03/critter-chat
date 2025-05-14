00905729c61707a9c6bd2c84c67a41d6ca0dbec8
jleib03 <leib@critter.pet>
2025-05-14 16:14:56 -0500

    Update user-info-form.tsx

--- components/user-info-form.tsx
+++ components/user-info-form.tsx
@@ -56,7 +56,7 @@ const UserInfoForm = forwardRef<UserInfoFormHandle, UserInfoFormProps>(({ select
       </div>
       <div className="bg-white rounded-b-lg p-6 shadow-sm flex-1 flex flex-col overflow-y-auto user-info-container">
         <p className="text-gray-700 mb-4 body-font">
-          Let's start by telling us a little bit about yourself, with first/last name and/or email. This lets Critter
+          Let's start by telling us a little bit about yourself, with your first name, last name, and email. This lets Critter
           match you to the services providers you already work with.
         </p>
 
